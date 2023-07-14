b53f21559abb80dfb288941f6bd91cf48e957bba
Gaurav Dhak <113551816+GauravDhak@users.noreply.github.com>
2023-07-14 10:10:15 +0530

    Update Web3Contract.sol

--- Web3Contract.sol
+++ Web3Contract.sol
@@ -1,7 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Web3Contract {
-  constructor() public {
+contract DreamCompanyContract {
+  address payable public student;
+  address payable public teacher;
+  address public dreamCompany;
+  uint public contractAmount;
+  bool public isHired;
+
+  constructor(address payable _student,address payable _teacher, address _dreamCompany, uint _contractAmount ){
+    student = _student;
+    teacher = _teacher;
+    dreamCompany = _dreamCompany;
+    contractAmount = _contractAmount;
+    isHired = false; 
+  }
+
+  function hire() external{
+    require(msg.sender == dreamCompany,"Only the dream company can trigger the hire function");
+    isHired = true;
+  }
+
+  function payTeacher() external{
+    require(isHired,"The student must be hired before making the payment");
+    teacher.transfer(contractAmount);
   }
 }
